9fd1989c439382b0b3ed03e0affae5fb189f3a92
Andrija Skontra <andrijaskontra@Andrijas-Mac-mini.local>
2024-12-01 15:16:48 +0100

    ADDED: fetch on free api to get list of random words

--- src/main.js
+++ src/main.js
@@ -4,6 +4,25 @@ const restartButton = document.getElementById("restart-button");
 const wpmResult = document.getElementById("wpm-result");
 let wpm;
 
+async function getWords() {
+  const wordListFn = await fetch(
+    "https://random-word-api.vercel.app/api?words=6",
+  )
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch(() => console.error("err, can't fetch data"));
+
+  const typerTextFromDB = wordListFn.reduce(
+    (accumulator, word) => accumulator + " " + word,
+    "",
+  );
+  return typerTextFromDB.trim();
+}
+
+const typerTextFromDB = await getWords();
+
+console.log(typerTextFromDB.trim());
+typerText.innerText = typerTextFromDB;
 let typerTextValue = typerText.innerText;
 let startedTime;
 
@@ -17,16 +36,18 @@ typingText.addEventListener("blur", (event) => {
 });
 
 typingText.addEventListener("input", (e) => {
-  if (e.target.value === typerTextValue) {
+  if (e.target.value === typerText.innerText) {
     wpm =
       typerTextValue.length /
       5 /
       ((new Date().getTime() - startedTime) / 60000);
     wpmResult.innerText = Math.floor(wpm) + " WPM";
+    typingText.style.background = "#d4ffeb";
   }
 });
 
-restartButton.addEventListener("click", (e) => {
+restartButton.addEventListener("click", async (e) => {
   typingText.value = "";
   wpmResult.innerText = "";
+  typerText.innerText = await getWords();
 });
